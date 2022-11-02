a1360cfc8d32fdc79103f0273841687076345745
William Andrews <41920314+WilliamAndrews1013@users.noreply.github.com>
2022-11-02 12:00:02 -0400

    commit

--- MMM-Qatar2022_Groups.js
+++ MMM-Qatar2022_Groups.js
@@ -1,33 +1,49 @@
 /**
- * @file MMM-RacePace-Predictor.js
+ * @file MMM-Qatar2022_Groups.js
  *
  * @author WilliamAndrews1013
  * @license ISC
  *
- * @see  https://github.com/WilliamAndrews1013/MMM-RacePace-Predictor
+ * @see  https://github.com/WilliamAndrews1013/MMM-Qatar2022_Groups
  */
-Module.register("MMM-RacePace-Predictor", {
+ 
+ 
+Module.register('MMM-Qatar2022_Groups', {
     // Set the minimum MagicMirror module version
     requiresVersion: "2.20.0",
-    // Default module config
+    
     defaults: {
-        
-    },
+	api_key: false
+    
 
-    loaded: function(callback) {
-		this.finishLoading();
-		Log.log(this.name + ' is loaded!');
-		callback();
-	},
+    loading: true,
 
-    getStyles: function(){
+    /*getStyles: function(){
         return ["font-awesome.css", "MMM-Qatar2022-Groups.css"]
-    },
+    },*/
 
+
+    // Module Properties
+    groups:[],
+    
     start() {
         Log.info(`Starting module: ${this.name}`);
-        this.sendSocketNotification('CONFIG', {config: this.config})
-    }
-
+        this.getData();
+    },
     
-})
+    getData: function () {
+	this.sendSocketNotification('GET_DATA',{this.config.aoi_key});
+    },
+    
+    socketNotificationReceived(notification, payload) {
+	if (notification === 'DATA') {
+	    console.log(payload)
+	}
+    },
+    
+    socketNotificationReceived(notification,payload) {
+	if (notification === 'ALL_MODULES_STARTED') {
+	    console.log('HI')
+	}
+    
+});
--- node_helper.js
+++ node_helper.js
@@ -1,123 +1,30 @@
 const fetch = require('node-fetch');
-const qs = require('querystring');
-const NodeHelper = require('node_helper');
+const NodeHelper = require('node-helper');
 
-const BASE_URL = 'http://api.cup2022.ir/api/v1';
 
-/**
- * Derived group details from API endpoint for easier usage.
- *
- * @typedef {object} Groups
- * @property {string} group_id - Group identifier
- * @property {string} _id - Each groups _id
- */
-
-/**
- * @typedef {object} Team
- * @property {number} team_id - Team identifier.
- * @property {string} name - Full team name.
- * @property {number} matches_played - Amout of matches played
- * @property {number} wins - Amount of wins
- * @property {number} losses - Amount of losses
- * @property {number} points - Amount of total points
- */
-
-/**
- * @module node_helper
- * @description Backend for the module to query data from the API provider.
- *
- * @requires external:node-fetch
- * @requires external:querystring
- * @requires external:node_helper
- */
 module.exports = NodeHelper.create({
-
-    /** @member {string} requiresVersion - Defines the minimum version of MagicMirror² to run this node_helper. */
-    requiresVersion: '2.20.0',
-
-    /**
-     * @function socketNotificationReceived
-     * @description Receives socket notifications from the module.
-     * @async
-     * @override
-     *
-     * @param {string} notification - Notification name
-     * @param {*} payload - Detailed payload of the notification.
-     *
-     * @returns {void}
-     */
-    async socketNotificationReceived(notification, payload) {
-        if (notification === 'CONFIG') {
-            this.config = payload.config;
-
-            await this.initGroups();
+    sendNotificationReceived(notification, payload) {
+        if (notification === 'GET_DATA') {
+            const url = 'http://api.cup2022.ir/api/v1/standings';
+            count options = {};
+            
+            if (payload.api_key) {
+                options.headers = {'Authorization': payload.api_key};
+            }
+            
+            this.getData(url, options);
         }
     },
-
-     /**
-     * @function initTeams
-     * @description Retrieves a list of all teams from the API and initializes teamMapping.
-     * @async
-     *
-     * @returns {void}
-     */
-    async initGroups() {
-        const data = await fetch(`${BASE_URL}/standings`, {
-            headers: {
-                Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzU4MGYzZWU2ODcyYWJmZTNiMGY5OWQiLCJpYXQiOjE2NjcyMzUxMzIsImV4cCI6MTY2NzMyMTUzMn0.6ZEPdApzNNyZk_esLOBzzWT1lXE7kFFk0Evmxr6blLY'
-            }
-        });
-
+    
+    getData = async(url, options) => {
+        const response = await fetch(url, options);
+        
         if (!response.ok) {
-            Log.error(`Initializing Groups failed: ${response.status} ${response.statusText}`);
-
+            console.error(`Getting Data: ${response.status} ${response.statusText}`);
             return;
         }
-
         
-
-        const groupA = data.filter((item) => {
-            return item.group === "A";
-        });
-
-        const groupB = data.filter((item) => {
-            return item.group === "B";
-        });
-
-        const groupC = data.filter((item) => {
-            return item.group === "C";
-        });
-
-        const groupD = data.filter((item) => {
-            return item.group === "D";
-        });
-
-        const groupE = data.filter((item) => {
-            return item.group === "E";
-        });
-
-        const groupF = data.filter((item) => {
-            return item.group === "F";
-        });
-
-        const groupG = data.filter((item) => {
-            return item.group === "G";
-        });
-
-        const groupH = data.filter((item) => {
-            return item.group === "H";
-        });
-
-        return [groupA, groupB, groupC, groupD, groupE, groupF, groupG, groupH];
-    },
-
-    async fetchData() {
-        const response = await fetch(`${BASE_URL}/standings`, {
-            headers: {
-                Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzU4MGYzZWU2ODcyYWJmZTNiMGY5OWQiLCJpYXQiOjE2NjcyMzUxMzIsImV4cCI6MTY2NzMyMTUzMn0.6ZEPdApzNNyZk_esLOBzzWT1lXE7kFFk0Evmxr6blLY'
-            }
-        });
-
-        console.log(response);
-    }
+        const parsedResponse = await response.json();
+        
+        this.sendSocketNotification('DATA', parsedResponse);
 });
